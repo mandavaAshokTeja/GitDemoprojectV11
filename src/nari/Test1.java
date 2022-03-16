788958a4fdbaa256afc1135b6edd417ee02e1d0f
Your Name <you@example.com>
2022-03-16 12:51:40 +0530

    This Is My First commit

---
+++ src/nari/Test1.java
@@ -0,0 +1,15 @@
+package nari;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		 if(hungry == "yes")
+			 System.out.println("eat something.....");
+		 else
+			 System.out.println("Do work.....");
+
+	}
+
+}
